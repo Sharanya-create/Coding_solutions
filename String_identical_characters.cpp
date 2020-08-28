e1d26d99f52da7d485ad69fb0d0fc770a2d7e3db
Sharanya S Bhat <59570348+Sharanya-create@users.noreply.github.com>
2020-08-28 17:23:38 +0530

    Create String_identical_characters.cpp

---
+++ String_identical_characters.cpp
@@ -0,0 +1,43 @@
+/*Problem Statement: Output the reduced string after removing k consecutive identical characters
+
+source: GeeksforGeeks */
+
+#include<iostream>
+using namespace std;
+
+typedef long int k1;
+int main(){
+  string s;
+  k1 top=-1;
+  k1 k;
+  cin>>k;
+  cin>>s;
+  char stack[s.length()];
+  k1 freq[s.length()];
+  stack[++top]=s[0];
+  freq[top]=1;
+  for(k1 i=1; s[i]!='\0';i++){
+      if(s[i]==stack[top]){
+          freq[top]++;
+          if(freq[top]==k){
+              top--;
+          }
+      }
+      else{
+          stack[++top]=s[i];
+          freq[top]=1;
+      }
+  }
+  if(top!=-1){
+  for(k1 i=0; i<=top; i++){
+      cout<<stack[i];
+  }
+  }
+  else{
+      cout<<"-1";
+  }
+  
+      
+  
+  return 0;
+}
